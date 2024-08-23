64f5fec809d7178b07b6099a0f81d9f5691d1473
haron-IV <bart-zen@protonmail.com>
2024-08-24 00:44:37 +0200

    auto token

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ setup:
 2. pnpm init-app // this step will delete all of your saved data (will create new file)
 
 running:
+
+1. pnpm dev // it will run dev server
+   if there is no access token for olx api it will run browser via puppert
+   if you werent logged in into your olx account in puppeter browser you have to login and reset app (tun pnpm dev)
+   then app will automaticaly can get your token when it expires (it does every few minutes)
+2. it will work :D
+3. in directory ./db you will have 3 files
+   data.json - your entire data
+   data.template.json - template for your data.json file
+   grouppedData.json - your data, but gruppet by configured property
+
+in ./userData directory is stored browser data from puppeteer. It has session, cookies, etc.
--- services/getToken/init.ts
+++ services/getToken/init.ts
@@ -1,16 +1,41 @@
 import p from "puppeteer"
+import { NotificationCenter } from "node-notifier"
+import path from "path"
+
+const nc = new NotificationCenter()
 
 export const getToken = async () => {
   const browser = await p.launch({
     headless: false,
     userDataDir: "./userData",
+    args: [`--window-size=1200,900`],
+    defaultViewport: {
+      width: 1200,
+      height: 900,
+    },
   })
   const page = await browser.newPage()
-  await page.goto("https://olx.pl/")
+  await page.goto("https://olx.pl")
   const cookies = await page.cookies()
   const accessToken = cookies.find(({ name }) => name === "access_token")
 
   process.env.TOKEN = accessToken?.value // set token to env
 
-  // await browser.close()
+  if (accessToken?.value === undefined) {
+    nc.notify(
+      {
+        title: "ACTION NEEDED",
+        message: "Browser showed. You have to log into your account to get token",
+        icon: path.join(process.cwd(), "alert_icon.png"),
+        sound: "",
+        actions: "ok",
+      },
+      async (_, __, metadata) => {
+        if (metadata?.activationValue === "ok") {
+          const btn = await page.$("a::-p-text(Twoje konto)")
+          await btn?.click()
+        }
+      },
+    )
+  } else browser.close()
 }
